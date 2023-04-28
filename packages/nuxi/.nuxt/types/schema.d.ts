37c6c568e9ed22ebc03aacf4b8f87f56d7eeda2a
June <1601745371@qq.com>
2023-04-28 12:09:35 +0800

    fix: nuxt prettire

--- packages/nuxi/.nuxt/types/schema.d.ts
+++ packages/nuxi/.nuxt/types/schema.d.ts
@@ -1,19 +1,29 @@
-import { NuxtModule } from 'nuxt/schema'
+import { NuxtModule } from 'nuxt/schema';
 declare module 'nuxt/schema' {
-  interface NuxtConfig {
-    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    modules?: (undefined | null | false | NuxtModule | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
-  }
-  interface RuntimeConfig {
-   app: {
-      baseURL: string,
+    interface NuxtConfig {
+        ['telemetry']?: typeof import('@nuxt/telemetry').default extends NuxtModule<
+            infer O
+        >
+            ? Partial<O>
+            : Record<string, any>;
+        modules?: (
+            | undefined
+            | null
+            | false
+            | NuxtModule
+            | string
+            | [NuxtModule | string, Record<string, any>]
+            | ['@nuxt/telemetry', Exclude<NuxtConfig['telemetry'], boolean>]
+        )[];
+    }
+    interface RuntimeConfig {
+        app: {
+            baseURL: string;
 
-      buildAssetsDir: string,
+            buildAssetsDir: string;
 
-      cdnURL: string,
-   },
-  }
-  interface PublicRuntimeConfig {
-
-  }
+            cdnURL: string;
+        };
+    }
+    interface PublicRuntimeConfig {}
 }
