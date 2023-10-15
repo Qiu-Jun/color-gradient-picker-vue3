206bb373a11f1919b15aed0e2a68432148b017a0
June <1601745371@qq.com>
2023-10-15 16:36:38 +0800

    fix: unexport rgbtohex

--- lib/index.ts
+++ lib/index.ts
@@ -4,10 +4,9 @@
  * @Author: June
  * @Date: 2023-03-18 00:33:21
  * @LastEditors: June
- * @LastEditTime: 2023-10-15 16:21:24
+ * @LastEditTime: 2023-10-15 16:36:08
  */
 import ColorPicker from './components/ColorPicker/index.vue'
 import './index.scss'
 import 'virtual:uno.css'
-export { default as rgbToHex } from '@l/helpers/rgbToHex'
 export default ColorPicker
