e72b2edc7e03b6a1577d1357132189a21ce467e4
June <1601745371@qq.com>
2024-12-05 12:28:06 +0800

    fix: 修改文件文字

--- lib/src/components/ColorPicker/components/Inputs/index.vue
+++ lib/src/components/ColorPicker/components/Inputs/index.vue
@@ -2,7 +2,7 @@
  * @Author: June
  * @Description: Description
  * @Date: 2024-12-04 11:43:15
- * @LastEditTime: 2024-12-05 11:44:38
+ * @LastEditTime: 2024-12-05 12:27:10
  * @LastEditors: June
 -->
 <template>
@@ -11,7 +11,7 @@
     <HSLInputs v-if="colorState.inputType === InputType.hsl" />
     <RGBInputs v-if="colorState.inputType === InputType.rgb" />
     <HSVInputs v-if="colorState.inputType === InputType.hsv" />
-    <CMKYInputs v-if="colorState.inputType === InputType.cmyk" />
+    <CMYKInputs v-if="colorState.inputType === InputType.cmyk" />
     <InputItem
       v-if="!colorState.hideOpacity"
       :input-val="colorState.hc.a * 100"
@@ -27,7 +27,7 @@ import {
   HSLInputs,
   RGBInputs,
   HSVInputs,
-  CMKYInputs,
+  CMYKInputs,
   InputItem,
 } from './components'
 import { useColor } from '@/hooks/useColor'
